1697de0678063091abe205abd485f71bb48796e4
Naufal Rizki <mnflrizki@gmail.com>
2023-10-20 16:47:43 +0700

    Master Backend Dicoding

---
+++ src/routes.js
@@ -0,0 +1,31 @@
+const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, updateNoteHandler, deleteNoteHandler } = require('./handler');
+
+const routes = [
+    {
+        method:'POST',
+        path:'/notes',
+        handler: addNoteHandler
+    },
+    {
+        method:'GET',
+        path:'/notes',
+        handler: getAllNotesHandler
+    },
+    {
+        method:'GET',
+        path:'/notes/{id}',
+        handler: getNoteByIdHandler
+    },
+    {
+        method:'PUT',
+        path:'/notes/{id}',
+        handler: updateNoteHandler
+    },
+    {
+        method:'DELETE',
+        path:'/notes/{id}',
+        handler: deleteNoteHandler
+    },
+]
+
+module.exports = routes
